53727d17daace5ec65ac81f5d7ed68b4635c3f15
DevynJamesRogers <devyn.j.rogers@gmail.com>
2020-02-27 08:00:42 -0500

    Add files via upload

---
+++ SE/src/Student.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+	private String fName;
+	private String lName;
+	private Schedule sc;
+	private ArrayList <String> pref;
+	
+	public Student(String f,String l,Schedule s, ArrayList <String> p) {
+		fName =f;
+		lName =l;
+		sc = s;
+		pref = p;
+	}
+	
+	public String toString() {
+		return "First: " +fName + " Last: " + lName + "\n\t\t\n" + sc.toString();
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public Schedule getSc() {
+		return sc;
+	}
+
+	public void setSc(Schedule sc) {
+		this.sc = sc;
+	}
+	public String listPrint() {
+		String str="";
+		for(int i =0; i < pref.size(); i++) {
+			str =str + pref.get(i); 
+		}
+		
+		return str;
+	}
+}
